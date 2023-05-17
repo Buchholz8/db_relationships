b8a8d86cafea9fbbd7b569dfe0ae335f3bde487a
markb <markbuchholz8@gmail.com>
2023-05-16 20:02:39 -0600

    added to sql files

--- data.sql
+++ data.sql
@@ -2,3 +2,21 @@ insert into citizen(name, address, phone_number, date_of_birth) values('carl' , 
 insert into citizen(name, address, phone_number, date_of_birth) values('frank' , '232 somewhere' , '780-385-0002' , '2004-11-12');
 
 insert into tax_account(citizen_id, date_created, account_number) values(2, '2005-02-02' , 312312)
+
+insert into car(release_year, model, make) values('2018-02-10' , 'model y' , 'sport');
+insert into car(release_year, model, make) values('2018-02-10' , 'model x' , 'race');
+insert into car(release_year, model, make) values('2018-02-10' , 'model z' , 'family');
+insert into car(release_year, model, make) values('2018-02-10' , 'model w' , 'basic');
+
+insert into student(name, date_of_birth) values('mike' , '2000-01-01');
+insert into student(name, date_of_birth) values('james' , '2000-04-09');
+insert into student(name, date_of_birth) values('jackson' , '2000-08-05');
+insert into student(name, date_of_birth) values('jamison' , '2000-12-03');
+
+insert into class(name, `time`) values('science' , '2:30');
+insert into class(name, `time`) values('math' , '1:30');
+
+insert into student_class(student_id , class_id) values(1,1);
+insert into student_class(student_id , class_id) values(2,1);
+insert into student_class(student_id , class_id) values(3,2);
+insert into student_class(student_id , class_id) values(4,2);
--- queries.sql
+++ queries.sql
@@ -16,63 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `citizen`
+-- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `citizen`;
+DROP TABLE IF EXISTS `student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `citizen` (
+CREATE TABLE `student` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  `address` varchar(30) DEFAULT NULL,
-  `phone_number` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
   `date_of_birth` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `citizen`
+-- Dumping data for table `student`
 --
 
-LOCK TABLES `citizen` WRITE;
-/*!40000 ALTER TABLE `citizen` DISABLE KEYS */;
-INSERT INTO `citizen` VALUES (1,'carl','222 somewhere','780-385-0001','2003-01-08'),(2,'frank','232 somewhere','780-385-0002','2004-11-12');
-/*!40000 ALTER TABLE `citizen` ENABLE KEYS */;
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'mike','2000-01-01'),(2,'jamison','2000-12-03'),(3,'jackson','2000-08-05'),(4,'james','2000-04-09');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `tax_account`
---
-
-DROP TABLE IF EXISTS `tax_account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tax_account` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `date_created` date DEFAULT NULL,
-  `account_number` int(11) DEFAULT NULL,
-  `citizen_id` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `tax_account_FK` (`citizen_id`),
-  CONSTRAINT `tax_account_FK` FOREIGN KEY (`citizen_id`) REFERENCES `citizen` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tax_account`
---
-
-LOCK TABLES `tax_account` WRITE;
-/*!40000 ALTER TABLE `tax_account` DISABLE KEYS */;
-INSERT INTO `tax_account` VALUES (1,'2005-02-02',312312,2);
-/*!40000 ALTER TABLE `tax_account` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'db_relationships'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -83,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-16 18:05:39
+-- Dump completed on 2023-05-16 20:01:38
